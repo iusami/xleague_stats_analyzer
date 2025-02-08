c8b9be734c679069f66313621f7ff4d51447369a
ippei usami <ippei_usami@caddi.jp>
2025-02-08 21:49:27 +0900

    apply format

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -38,7 +38,12 @@ def test_load_config_from_file(mock_config, mock_file):
     assert result.run_long_gain_threshold == 10
     assert result.pass_long_gain_threshold == 20
 
-@patch("builtins.open", new_callable=mock_open, read_data='{"teams":[{"name":"Team A"},{"name":"Team B"}],"abbreviation":"TA"}')
+
+@patch(
+    "builtins.open",
+    new_callable=mock_open,
+    read_data='{"teams":[{"name":"Team A"},{"name":"Team B"}],"abbreviation":"TA"}',
+)
 def test_load_team_names_from_file(mock_file):
     file_path = Path("/path/to/teams.json")
     team_names, team_abbreviation = load_team_names_from_file(file_path)
@@ -55,6 +60,7 @@ def test_load_team_names_from_file_file_not_found(mock_file):
 
     with pytest.raises(FileNotFoundError):
         load_team_names_from_file(file_path)
+
 
 if __name__ == "__main__":
     pytest.main()
